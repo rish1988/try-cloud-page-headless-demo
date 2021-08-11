5ab65657f27cc2e7b78cb63e6b58d332a67e5845
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2021-08-11 20:08:25 +0300

    update netlify build command

--- get-env-netlify.js
+++ get-env-netlify.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-let envName = process.env.HEAD.replace("env/", "");
+let envName = process.env.BRANCH.replace("env/", "");
 
 require('fs')
     .writeFileSync(".config-env", `REACT_APP_CONFIG_ENV=environments/${envName}`);
